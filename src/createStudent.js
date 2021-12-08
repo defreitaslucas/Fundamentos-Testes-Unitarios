83aec88baecf35b6c8fc3618a688cf095f9c00e7
LucasTapa <lucas.dfa@live.com>
2021-12-08 17:27:06 -0300

    requisito 5 feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let estudante = {
+    name: nome,
+  };
+  estudante.feedback = () => 'Eita pessoa boa!';
+  return estudante;
+};
 
 module.exports = createStudent;
